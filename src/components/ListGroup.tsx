6434ad33018e979bbdadb7245c5cdb15d76769b3
KishanY17 <105463218+KishanY17@users.noreply.github.com>
2023-08-24 00:06:02 +1200

    Updating Dashboard

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,16 @@
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">Dashboard</li>
-      <li className="list-group-item">Claims</li>
-      <li className="list-group-item">Inventory</li>
-      <li className="list-group-item">Mailbox</li>
-      <li className="list-group-item">Invoices</li>
-      <li className="list-group-item">Report</li>
-    </ul>
+    <>
+      <h1>ManageMe</h1>
+      <ul className="list-group">
+        <li className="list-group-item">Dashboard</li>
+        <li className="list-group-item">Claims</li>
+        <li className="list-group-item">Inventory</li>
+        <li className="list-group-item">Mailbox</li>
+        <li className="list-group-item">Invoices</li>
+        <li className="list-group-item">Report</li>
+      </ul>
+    </>
   );
 }
 
