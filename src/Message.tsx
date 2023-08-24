454cf78afda0fd26c36a231beeecb46ac0abc50f
Sam Cathro <zjq6910@autuni.ac.nz>
2023-08-24 14:39:50 +1200

    Removing the old side nav and replacing it with the updated one

--- src/Message.tsx
+++
@@ -1,9 +0,0 @@
-function Message(){
-
-    const name = 'Kishan'
-    if(name)
-        return <h1>Welcome to ManageMe {name}</h1>;
-    return <h1>Welcome to ManageMe Friend</h1>
-}
-
-export default Message;
