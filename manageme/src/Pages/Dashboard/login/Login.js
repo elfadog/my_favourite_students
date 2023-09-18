858d17bb3dc692c96e55556143980e159349e806
Sam Cathro <zjq6910@autuni.ac.nz>
2023-09-18 19:32:31 +1200

    fixed the page background loading in weirdly

--- manageme/src/Pages/Dashboard/login/Login.js
+++ manageme/src/Pages/Dashboard/login/Login.js
@@ -1,27 +1,27 @@
-// // Login.js
-// import React from 'react';
-// import './Login.css';
+// // // Login.js
+// // import React from 'react';
+// // import './Login.css';
 
-// function Login() {
-//   return (
-//     <div className="login-container">
-//       <div className="login-box">
-//         <h2>ManageMe</h2>
-//         <form>
-//           <div className="form-group">
-//             <input type="text" id="username" name="username" placeholder="Username" />
-//           </div>
-//           <div className="form-group">
-//             <input type="password" id="password" name="password" placeholder="Password" />
-//           </div>
-//           <button type="submit">Login</button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
+// // function Login() {
+// //   return (
+// //     <div className="login-container">
+// //       <div className="login-box">
+// //         <h2>ManageMe</h2>
+// //         <form>
+// //           <div className="form-group">
+// //             <input type="text" id="username" name="username" placeholder="Username" />
+// //           </div>
+// //           <div className="form-group">
+// //             <input type="password" id="password" name="password" placeholder="Password" />
+// //           </div>
+// //           <button type="submit">Login</button>
+// //         </form>
+// //       </div>
+// //     </div>
+// //   );
+// // }
 
-// export default Login;
+// // export default Login;
 
 import React, { useEffect } from 'react';
 import gsap from 'gsap'; // Import GSAP
@@ -40,7 +40,7 @@ function Login() {
     const tl = gsap.timeline();
 
     // Add animations to the timeline
-    tl.to(svg, { duration: 1, scale: 1.5, opacity: 0, ease: 'power1.in' })
+    tl.to(svg, { duration: 0.5, scale: 1.5, opacity: 0, ease: 'power1.in' })
       .to(loginBox, { duration: 1, opacity: 1, ease: 'power1.out' });
 
     // Play the animation timeline
@@ -67,3 +67,5 @@ function Login() {
 }
 
 export default Login;
+
+
