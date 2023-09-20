7671004e48c1c0afea7e4da0d1354f22275d8fc2
Sam Cathro <zjq6910@autuni.ac.nz>
2023-09-21 08:34:11 +1200

    Tidy up did not work
    just added the fonts to the parent directory

--- manageme/src/Pages/Dashboard/Dashboard/Dashboard.js
+++ manageme/src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -13,32 +13,14 @@ function Dashboard() {
     "Invoices",
     "Reports",
   ];
-  //   useEffect(() => {
-  //     // Select the login box and SVG elements
-  //     const loginBox = document.querySelector('.login-box');
-  //     const svg = document.querySelector('.background-svg');
-
-  //     // Hide the login box initially
-  //     gsap.set(loginBox, { opacity: 0 });
-
-  //     // Create an animation timeline
-  //     const tl = gsap.timeline();
-
-  //     // Add animations to the timeline
-  //     tl.to(svg, { duration: 0.5, scale: 1.5, opacity: 0, ease: 'power1.in' })
-  //       .to(loginBox, { duration: 1, opacity: 1, ease: 'power1.out' });
-
-  //     // Play the animation timeline
-  //     tl.play();
-  //   }, []);
 
   return (
     <div className="dashboard-container">
       <div className="banner">
         <h2>ManageMe</h2>
+        <ListGroup />
         <p>account</p>
       </div>
-      <ListGroup />
       <div className="main">
         <p>main content</p>
       </div>
