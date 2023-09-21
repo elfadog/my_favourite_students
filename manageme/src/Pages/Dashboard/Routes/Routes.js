85c76ee0e95148ad8a755799cd995b08ff93d449
wmq4940 <wmq4940@autuni.ac.nz>
2023-09-21 12:54:55 +1200

    route.js file
    also updated the .gitignore

--- manageme/src/Pages/Dashboard/Routes/Routes.js
+++ manageme/src/Pages/Dashboard/Routes/Routes.js
@@ -1,4 +1,18 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import Dashboard from './Dashboard';
+import Support from './Support';
+
+const path = window.location.pathname
+
+ReactDOM.render(
+  <React.StrictMode>
+    {path.indexOf('/Support') === -1? <Dashboard/> : < Support/>}
+  </React.StrictMode>
+);
 
 
 export default function Routes() {
