561a699739a810b071336e2fb9b958685516f837
chuiiboi <brendancroft2003@gmail.com>
2023-09-21 23:27:50 +1200

    updated dashboard too look more like figma design
    changed colours, sizes, fonts, ratios and added icons for the menu options.

--- manageme/src/Pages/Dashboard/Dashboard/Dashboard.js
+++ manageme/src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
 import "./Dashboard.css";
 import logo from "./logo.png";
+import dashboardIcon from "./Icons/dashboardIcon.png";
+import claimsIcon from "./Icons/claimsIcon.png";
+import inventoryIcon from "./Icons/inventoryIcon.png";
+import mailboxIcon from "./Icons/mailboxIcon.png";
+import invoicesIcon from "./Icons/invoicesIcon.png";
+import reportsIcon from "./Icons/reportsIcon.png";
+// import supervisedUserIcon from "./Icons/supervisedUserIcon.png";
+
+const menuItems = [
+  { text: "Dashboard", icon: dashboardIcon },
+  { text: "Claims", icon: claimsIcon },
+  { text: "Inventory", icon: inventoryIcon },
+  { text: "Mailbox", icon: mailboxIcon },
+  { text: "Invoices", icon: invoicesIcon },
+  { text: "Reports", icon: reportsIcon },
+];
 
 // Sidebar component
 function Sidebar({ isOpen, toggleSidebar }) {
-  const menuItems = ["Dashboard", "Claims", "Inventory", "Mailbox", "Invoices", "Reports"];
-
   return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
       <ul>
         {menuItems.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            <div className="menu-item">
+              <div
+                className="menu-icon"
+                style={{ backgroundImage: `url(${item.icon})` }}
+              ></div>
+              <span>{item.text}</span>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
@@ -27,23 +49,32 @@ function Dashboard() {
   return (
     <div className="dashboard-container">
       {/* Sidebar */}
-        <div className="banner">
-          <div className="left-banner">
-            <button onClick={toggleSidebar} style={{backgroundImage: `url(${logo})`}}></button>
-            <h2>ManageMe</h2>
-          </div>
-          <div className="right-banner">
-            <p>account</p>
-          </div>
+      <div className="banner">
+        <div className="left-banner">
+          <button
+            className="invert-effect"
+            onClick={toggleSidebar}
+            style={{ backgroundImage: `url(${logo})` }}
+          ></button>
+          <h2>ManageMe</h2>
         </div>
-        <div className="main">
-        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-  <div className={`content ${isSidebarOpen ? 'open' : ''}`}>
-    <p>content</p>
-          </div>
+        <div className="right-banner">
+          <p>account</p>
         </div>
       </div>
+      <div className="main">
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <div className={`content ${isSidebarOpen ? "open" : ""}`}>
+          <p>content</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default Dashboard;
+
+// TODO:
+// 1: add objects to 'content' area
+// 2: add the account icon with a dropdown that contains(account setting and logout options)
+// 3: make the menu items highlight depending on which page is selected (Managing State)
--- manageme/src/Pages/Dashboard/login/Login.js
+++ manageme/src/Pages/Dashboard/login/Login.js
@@ -1,47 +1,51 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import gsap from 'gsap';
-import './Login.css';
-import logo from './logo.png';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+import gsap from "gsap";
+import "./Login.css";
+import logo from "./logo.png";
 
 const Login = () => {
   const navigate = useNavigate(); // Get the navigate function
   useEffect(() => {
-    const loginBox = document.querySelector('.login-box');
-    const svg = document.querySelector('.background-svg');
+    const loginBox = document.querySelector(".login-box");
+    const svg = document.querySelector(".background-svg");
     gsap.set(loginBox, { opacity: 0 });
 
     const tl = gsap.timeline();
-    tl.to(svg, { duration: 0.5, scale: 1.5, opacity: 0, ease: 'power1.in' })
-      .to(loginBox, { duration: 1, opacity: 1, ease: 'power1.out' });
+    tl.to(svg, {
+      duration: 0.5,
+      scale: 1.5,
+      opacity: 0,
+      ease: "power1.in",
+    }).to(loginBox, { duration: 1, opacity: 1, ease: "power1.out" });
     tl.play();
   }, []);
   const handleLoginClick = () => {
     // You can also add your login logic here
-    navigate('/Dashboard'); // Navigate to the dashboard when the button is clicked
+    navigate("/Dashboard"); // Navigate to the dashboard when the button is clicked
   };
 
   return (
     <div className="login-container">
       <div className="login-box">
-      <img src={logo} alt="Logo" className="logo" />
+        <img src={logo} alt="Logo" className="logo" />
         <h2>ManageMe</h2>
         <form>
           <div className="form-group">
-            <label htmlFor='username'>USERNAME:</label>
-            <input type="text" id="username" name="username"/>
+            <label htmlFor="username">USERNAME:</label>
+            <input type="text" id="username" name="username" />
           </div>
           <div className="form-group">
-            <label htmlFor='password'>PASSWORD:</label>
-            <input type="password" id="password" name="password"/>
+            <label htmlFor="password">PASSWORD:</label>
+            <input type="password" id="password" name="password" />
           </div>
         </form>
-        <button type="button" onClick={handleLoginClick}>Login</button>
+        <button type="button" onClick={handleLoginClick}>
+          Login
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
-
-
