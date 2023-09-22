c6af9ff94a17694759e0b4fb312c613120482d14
KishanY17 <105463218+KishanY17@users.noreply.github.com>
2023-09-22 14:56:57 +1200

    Adding in FAQs

--- manageme/src/Pages/Dashboard/Support/Support.js
+++ manageme/src/Pages/Dashboard/Support/Support.js
@@ -2,11 +2,23 @@ import React from 'react';
 import Icon from './Icon';
 
 const Support = () => {
-    return (
-      <div>
-        <Icon/>
-      </div>
-    )
-}
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', position: 'absolute', top: '50px', left: '30px' }}>
+      <div>Frequently Asked Questions</div>
+      <Icon />
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        <li>
+          <strong>Q: How do I contact customer support?</strong>
+          <p>A: You can contact our customer support team by emailing support@example.com or by calling +1-123-456-7890.</p>
+        </li>
+        <li>
+          <strong>Q: What are your business hours?</strong>
+          <p>A: Our business hours are from 9:00 AM to 5:00 PM, Monday through Friday.</p>
+        </li>
+        {/* Add more FAQs and answers as needed */}
+      </ul>
+    </div>
+  );
+};
 
-export default Support
+export default Support;
