1174605e1de4e1cf4a0ab54ce85d1c12d5d5b713
KishanY17 <105463218+KishanY17@users.noreply.github.com>
2023-09-22 19:10:12 +1200

    Support page update

--- manageme/src/Pages/Dashboard/Support/Support.js
+++ manageme/src/Pages/Dashboard/Support/Support.js
@@ -27,29 +27,20 @@ const Support = () => {
         </li>
         <li>
           <div className="faq-container">
-            <strong>Q: What are your business hours?</strong>
-            <p>A: Our business hours are from 9:00 AM to 5:00 PM, Monday through Friday.</p>
+            <strong>Q: What are the prices like?</strong>
+            <p>A: Our prices vary as it relies on multiple different factors. Contact us official at "manageme@support.com" for an accurate pricing.</p>
           </div>
         </li>
         <li>
           <div className="faq-container">
-            <strong>Q: What are your business hours?</strong>
-            <p>A: Our business hours are from 9:00 AM to 5:00 PM, Monday through Friday.</p>
+            <strong>Q: How long are the free trials</strong>
+            <p>A: Free trials are typically 30 days.</p>
           </div>
-        </li><li>
+        </li>
+        <li>
           <div className="faq-container">
-            <strong>Q: What are your business hours?</strong>
-            <p>A: Our business hours are from 9:00 AM to 5:00 PM, Monday through Friday.</p>
-          </div>
-        </li><li>
-          <div className="faq-container">
-            <strong>Q: What are your business hours?</strong>
-            <p>A: Our business hours are from 9:00 AM to 5:00 PM, Monday through Friday.</p>
-          </div>
-        </li><li>
-          <div className="faq-container">
-            <strong>Q: What are your business hours?</strong>
-            <p>A: Our business hours are from 9:00 AM to 5:00 PM, Monday through Friday.</p>
+            <strong>Q: What happens after the free trial period</strong>
+            <p>A: You will be notified that you trial has ended and you will need to make a subscription to contine using the app.</p>
           </div>
         </li>
         {/* Add more FAQs and answers as needed */}
