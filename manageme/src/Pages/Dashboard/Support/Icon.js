b7c7686ca257fe194d432f00412d914e86b69fd7
KishanY17 <105463218+KishanY17@users.noreply.github.com>
2023-09-22 12:16:12 +1200

    fixing up the chat icon

--- manageme/src/Pages/Dashboard/Support/Icon.js
+++ manageme/src/Pages/Dashboard/Support/Icon.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import './Icon.css'; 
 
-const Icon = props =>{
-    return(
-        <div style = {{position: 'fixed', bottom: '30px', right: '30px'}}>
-            <div>Need Help?</div>
-            <div className="element-container">
-            <div className="circle"></div>
-            <div className="element"></div>
-        </div>
+const Icon = (props) => {
+  return (
+    <div style={{ position: 'fixed', bottom: '30px', right: '30px' }}>
+      <div className="element-container">
+        <div className="circle"></div>
+        <div className="element"></div>
+        <div className="text">Need Help?</div>
+      </div>
     </div>
-    );
+  );
 }
 
-export default Icon
+export default Icon;
