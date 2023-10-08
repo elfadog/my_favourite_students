206aa335c3e8069290697c8b018306ed9b1f07a9
Sam Cathro <zjq6910@autuni.ac.nz>
2023-10-08 13:45:07 +1300

    Added mongoDB to the global
    added change to mongoDB for all users. Made a mistake that took hours to fix.
    use admin, admin to access the data. Ensure to run npm install before trying to run both.
    Add these lines to your package.json file " "server": "cd server && node server.js",
        "dev": "concurrently --kill-others-on-fail \"npm run server\" \"npm run client\"""
    Need to run from one root level higher. Contact me if you need clarification.

--- server/server.js
+++ server/server.js
@@ -1,48 +1,50 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+// const loginRoutes = require('./Routes/loginRoute');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mydb')
-.then(
-    () => { 
-      console.log("Connected");
-    },
-    err => {
-      console.log(err); 
-    }
-);
+const MONGO_URI = process.env.MONGO_URI || "fallbackURI";
+// const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://samuelcathro:VWr32%40%29%21%5E%21%21@myfavouritestudents.ghugjzv.mongodb.net/manageMe?retryWrites=true&w=majority"; // Use environment variable or a fallback
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { 
+        console.log("Connected to MongoDB");
+    })
+    .catch(err => {
+        console.error("Error connecting to MongoDB:", err);
+    });
 
 const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
 });
 
-const User = mongoose.model('User', UserSchema, 'mydb');
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+const User = mongoose.model('User', UserSchema, 'Users');
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
+
+// app.use('/api', loginRoutes);
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     
     try {
-      const user = await User.findOne({ username });
-      
-      if (!user || user.password !== password) {
-        return res.status(401).json({ success: false, error: 'Invalid username or password' });
-      }
-      
-      res.status(200).json({ success: true, message: 'Login successful' });
+        const user = await User.findOne({ username });
+        
+        if (!user || user.password !== password) {
+            return res.status(401).json({ success: false, error: 'Invalid username or password' });
+        }
+        res.status(200).json({ success: true, message: 'Login successful' });
     } catch (err) {
-      console.error('Server error:', err);
-      res.status(500).json({ success: false, error: 'Server error' });
+        console.error('Server error:', err);
+        res.status(500).json({ success: false, error: 'Server error' });
     }
 });
 
-app.listen(3001, () => console.log('Server is running on port 3001'));
+app.listen(3001, '0.0.0.0', () => console.log('Server is running on port 3001'));
